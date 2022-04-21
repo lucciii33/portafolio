9c6cf907cff60848fc2a1fac1e2572f3500eca50
lucciii33 <angelomaiele@gmail.com>
2022-04-21 19:49:44 +0000

    hard work

--- src/js/component/aboutme.js
+++ src/js/component/aboutme.js
@@ -3,17 +3,19 @@ import { Link } from "react-router-dom";
 import Tilt from 'react-vanilla-tilt'
 export const AboutMe = () => {
 	return (
+		<div>
 		<div className="d-flex justify-content-center" style={{backgroundColor: "#4825C7"}}>
 			<div className="conta2" >
 				<Tilt style={{backgroundColor: "rgba(225,225,225,0.1)", borderRadius: '15px'}}>
 				<div className="card2">
 				<div className="content text-center">
 				<h2>About me</h2>
-				<p>Lorem ipsu viene otra vez y se siente feliz de estar aqui y no se que mas vegra poner aca pero tiene que ser largo pa que se vea bien y depsues marico el que lo lea</p>
+				<p>Lorem ipsu viene otra vez y se siente feliz de estar aqui y no se que mas vegra poner aca pero tiene que ser largo pa que se vea bien y depsues marico el que lo lea, al parecer todavia esta verga es corta y se supone que deberia ser mas larga so aqui vamossss! punetaaaa marico el que lo leaaaaa!</p>
 				</div>
 				</div>
 		</Tilt>
 			</div>
+		</div>
 		</div>
 		
 	);
