4b796864458f1e94300cf249d2ec5fd48b65dcac
lucciii33 <angelomaiele@gmail.com>
2022-04-21 20:05:40 +0000

    working about me

--- src/js/component/aboutme.js
+++ src/js/component/aboutme.js
@@ -3,8 +3,14 @@ import { Link } from "react-router-dom";
 import Tilt from 'react-vanilla-tilt'
 export const AboutMe = () => {
 	return (
-		<div>
-		<div className="d-flex justify-content-center" style={{backgroundColor: "#4825C7"}}>
+		<div className=""style={{backgroundColor: "#4825C7"}}>
+			<div >
+			<div className="text-center p-0">
+			 <h2 className='title'>About me</h2>
+
+			</div>
+			</div>
+			<div className="d-flex justify-content-center" style={{backgroundColor: "#4825C7"}}>
 			<div className="conta2" >
 				<Tilt style={{backgroundColor: "rgba(225,225,225,0.1)", borderRadius: '15px'}}>
 				<div className="card2">
@@ -13,7 +19,7 @@ export const AboutMe = () => {
 				<p>Lorem ipsu viene otra vez y se siente feliz de estar aqui y no se que mas vegra poner aca pero tiene que ser largo pa que se vea bien y depsues marico el que lo lea, al parecer todavia esta verga es corta y se supone que deberia ser mas larga so aqui vamossss! punetaaaa marico el que lo leaaaaa!</p>
 				</div>
 				</div>
-		</Tilt>
+		 </Tilt>
 			</div>
 		</div>
 		</div>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -6,7 +6,7 @@ export const Home = () => (
 	<div className=" " style={{backgroundColor: "#4825C7"}}>
 		<div className="text-center">
 
-		<h1 className="title mb-2"><strong>Angelo Maiele</strong></h1>
+		<h1 className="title "><strong>Angelo Maiele</strong></h1>
 		<h2 className="title">Full Stack Developer</h2>
 		</div>
 		<div className="objd">
