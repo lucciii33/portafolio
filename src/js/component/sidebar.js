7baa6016d3ef692aa7f777a03636d11900550f94
lucciii33 <angelomaiele@gmail.com>
2022-05-04 15:11:59 +0000

    fixed navbar

--- src/js/component/sidebar.js
+++ src/js/component/sidebar.js
@@ -5,43 +5,43 @@ import { ImHome } from "react-icons/im";
 import { RiCodeSSlashFill, RiComputerFill } from "react-icons/ri"
 
 export const Sidebar = () => {
-	const [colorButton, setColorButton] = useState("iconState");
-	const [colorButton1, setColorButton1] = useState("iconState");
-	const [colorButton2, setColorButton2] = useState("iconState");
-	const [colorButton3, setColorButton3] = useState("iconState");
-	const [colorButton4, setColorButton4] = useState("iconState");
+	const [colorButton, setColorButton] = useState("");
+	// const [colorButton1, setColorButton1] = useState("iconState");
+	// const [colorButton2, setColorButton2] = useState("iconState");
+	// const [colorButton3, setColorButton3] = useState("iconState");
+	// const [colorButton4, setColorButton4] = useState("iconState");
 	return (
 		<nav className="" style={{backgroundColor: "#4825C7"}}>
 			<div className="sidebar ">
 
 			<div className="containerIcon ">
 			{/* <Link to="/" className="text-decoration-none"> */}
-			<a className={colorButton=="iconState"?"icon": colorButton1=="iconState"?"iconState":"icon" } onClick={()=>{
-				setColorButton("icon"); 
+			<a className={colorButton=="home"?"iconWhite":"icon" } onClick={()=>{
+				setColorButton("home"); 
 			}} href='#home'><ImHome size={30}/></a>
 			{/* </Link> */}
 			</div>
 			
 			<div className="containerIcon ">
 			{/* <Link to="/aboutme" className="text-decoration-none" > */}
-			<a className={colorButton1 == "iconState"? "icon": colorButton2=="iconState"?"iconState":"icon"  } onClick={()=>{
-				setColorButton1("icon");
+			<a className={colorButton=="about"?"iconWhite":"icon" } onClick={()=>{
+				setColorButton("about");
 			}} href='#about'><MdSettingsAccessibility size={30}/></a>
 			{/* </Link> */}
 			</div>
 
 			<div className="containerIcon  ">
 			{/* <Link to="/Cards" className=" text-decoration-none"> */}
-			<a className={colorButton2 == "iconState"? "icon": colorButton3=="iconState"?"iconState":"icon" } onClick={()=>{
-				setColorButton2("icon");
+			<a className={colorButton=="cards"?"iconWhite":"icon" } onClick={()=>{
+				setColorButton("cards");
 			}} href='#cards'><RiComputerFill size={30}/></a>
 			{/* </Link> */}
 			</div>
 
 			<div className="containerIcon  ">
 			{/* <Link to="/Cards" className=" text-decoration-none"> */}
-			<a className={colorButton3 == "iconState"? "icon": colorButton4=="iconState"?"iconState":"icon"  } onClick={()=>{
-				setColorButton3("icon");
+			<a className={colorButton=="tools"?"iconWhite":"icon" } onClick={()=>{
+				setColorButton("tools");
 			}} href='#tools'><RiCodeSSlashFill size={30}/></a>
 			{/* </Link> */}
 			</div>
@@ -50,8 +50,8 @@ export const Sidebar = () => {
 			
             <div className="containerIcon ">
 			{/* <Link to="/" className="text-decoration-none"> */}
-			<a className={colorButton4 == "iconState"? "icon" :"iconState" } onClick={()=>{
-				setColorButton4("icon");
+			<a className={colorButton=="contact"?"iconWhite":"icon" } onClick={()=>{
+				setColorButton("contact");
 			}} href='#contact'><MdAttachEmail size={30}/></a>
 			{/* </Link> */}
 			</div>
