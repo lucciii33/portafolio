f4a6a62395af0d99fcec372211a114dd25a6fd82
lucciii33 <angelomaiele@gmail.com>
2022-06-20 15:35:17 +0000

    working button

--- src/js/component/contact.js
+++ src/js/component/contact.js
@@ -2,36 +2,59 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Tilt from 'react-vanilla-tilt'
 import Spline from '@splinetool/react-spline';
+import { useState, useContext, } from "react";
+import { Context } from "../store/appContext";
 export const Contact = () => {
+	const { store, actions } = useContext(Context);
+
+	const [contact, setContact] = useState({ name: "", email: "", message: "" });
+	const handleChange = e => {
+		setContact({ ...contact, [e.target.name]: e.target.value });
+		console.log(contact);
+	};
 	return (
-		<div className=""style={{backgroundColor: "#4825C7"}}>
+		<div className="" style={{ backgroundColor: "#4825C7" }}>
 			<div>
-			 <a   href="https://www.linkedin.com/in/angelo-maiele-68626333/details/experience/"> 
-			 <Spline scene="https://prod.spline.design/EZl1sN5G3qRCksA2/scene.spline" /></a>
-			
-            </div> 
-			<div id="contact" >
-			<div className="text-center p-0" id="aboutme">
-			 <h2  className='title'>Contact me</h2>
+				<a href="https://www.linkedin.com/in/angelo-maiele-68626333/details/experience/">
+					<Spline scene="https://prod.spline.design/EZl1sN5G3qRCksA2/scene.spline" /></a>
+
 			</div>
+			<div id="contact" >
+				<div className="text-center p-0" id="aboutme">
+					<h2 className='title'>Contact me</h2>
+				</div>
 			</div>
 
-			<div className="d-flex justify-content-center" style={{backgroundColor: "#4825C7"}}>
-			<div className="conta3" >
-				<Tilt style={{backgroundColor: "rgba(225,225,225,0.1)", borderRadius: '15px' }}>
-				<div className="card3 d-flex flex-column ">
-				<div className="content text-center d-grid mx-auto">
-				<input type="text" placeholder="Your name"></input>
-				<input type="Password" placeholder="Password"></input>
-				<textarea placeholder="Tell me about your dream website "></textarea>
+			<div className="d-flex justify-content-center" style={{ backgroundColor: "#4825C7" }}>
+				<div className="conta3" >
+					<Tilt style={{ backgroundColor: "rgba(225,225,225,0.1)", borderRadius: '15px' }}>
+						<div className="card3 d-flex flex-column ">
+							<div className="content text-center d-grid mx-auto">
+								<input type="text" 
+								placeholder="Your name" 
+								name="name"
+								value={contact.name}
+								onChange={handleChange}></input>
+								<input type="text" 
+								placeholder="Email"
+								name="email"
+								value={contact.email}
+								onChange={handleChange}></input>
+								<textarea placeholder="Tell me about your dream website"
+								name="message"
+								value={contact.message}
+								onChange={handleChange}></textarea>
+								<button onClick={()=>{
+									actions.saveMessage(contact.email,contact.name,contact.message);
+								}}>send me to back end</button>
+							</div>
+						</div>
+					</Tilt>
 				</div>
-				</div>
-		 </Tilt>
 			</div>
+			<br />
+			<br />
 		</div>
-		<br/>
-		<br/>
-		</div>
-		
+
 	);
 };
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,43 +1,35 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			messages: []
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			saveMessage: (email,name, message) => {
+				//   let favoriteString = favorites.toString();
+				  fetch("https://3000-lucciii33-backendportaf-hpbzlbv07nz.ws-us47.gitpod.io/contactme", {
+					method: "POST",
+					headers: {
+					  "Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						email,
+						message,
+						name,
+					}),
+				  })
+					.then((response) => response.json())
+					.then((data) =>{
+						console.log(data)
+						if(data){
+							alert("thank you for your message")
+						}
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
+					} )
+		
+					.catch((err) => console.log(err));
+				},
+				
 		}
 	};
 };
